1afd08d3e141487529d1c79264fc5e35a465f84f
bmoharana95 <bmoharana95@gmail.com>
2023-05-22 12:29:40 +0530

    design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [result, setResult] = useState('')
+  const cleardata = () => {
+    setResult = '';
+  }
+  const displayvalue = (value) => {
+    setResult((prevdata) => prevdata + value)
+  }
+  const equalvalue = () => {
+    setResult((prevdata) => eval(prevdata));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">CALCULATOR</div>
+      <table>
+        <tbody>
+          <tr>
+            <td colSpan="3"><input type="text" id="result" value={result} readOnly /></td>
+            <td><input type="button" value="c" onClick={cleardata} readOnly /></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="1" onClick={() => displayvalue('1')} readOnly /></td>
+            <td><input type="button" value="2" onClick={() => displayvalue('2')} readOnly /></td>
+            <td><input type="button" value="3" onClick={() => displayvalue('3')} readOnly /></td>
+            <td><input type="button" value="/" onClick={() => displayvalue('/')} readOnly /></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="4" onClick={() => displayvalue('4')} readOnly /></td>
+            <td><input type="button" value="5" onClick={() => displayvalue('5')} readOnly /></td>
+            <td><input type="button" value="6" onClick={() => displayvalue('6')} readOnly /></td>
+            <td><input type="button" value="-" onClick={() => displayvalue('-')} readOnly /></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="7" onClick={() => displayvalue('7')} readOnly /></td>
+            <td><input type="button" value="8" onClick={() => displayvalue('8')} readOnly /></td>
+            <td><input type="button" value="9" onClick={() => displayvalue('9')} readOnly /></td>
+            <td><input type="button" value="+" onClick={() => displayvalue('+')} readOnly /></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="." onClick={() => displayvalue('.')} readOnly /></td>
+            <td><input type="button" value="0" onClick={() => displayvalue('0')} readOnly /></td>
+            <td><input type="button" value="=" onClick={() => equalvalue('=')} readOnly /></td>
+            <td><input type="button" value="*" onClick={() => displayvalue('*')} readOnly /></td>
+          </tr>
+
+
+        </tbody>
+
+      </table>
+
     </div>
   );
 }
 
 export default App;
+
